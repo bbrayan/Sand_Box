18e117f1943153698a282cd1a1b86c10ce658352
Brayan Boukhman <brayanboukhman@yahoo.ca>
2021-02-09 00:02:49 -0500

    first push

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+import './Chat.css';
+
+import InfoBar from '../InforBar/InforBar'
+import Input from '../Input/Input'
+import Messages from '../Messages/Messages'
+let socket;
+
+
+const Chat = ({ location }) =>{
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    const ENDPOINT = 'localhost:5000'
+
+    useEffect(() =>{
+        const { name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT, {transports: ['websocket', 'polling', 'flashsocket']});
+        
+        setName(name);
+        setRoom(room);
+
+        socket.emit('join', { name, room }, () => {
+           
+        });
+
+        return () =>{
+            socket.emit('disconnect');
+            socket.off();
+        }
+    }, [ENDPOINT, location.search])
+    //function to recieve messages
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages(messages => [ ...messages, message ]);
+            console.log(message);
+        })
+    }, []) 
+    // note reason for bug was that it would setup up a listener everytime a message was recieved [messages] to [] fixed it.
+
+    //function for sending messages
+    const sendMessage = (event) => {
+        //prevent the page from refreshes
+        console.log(message);
+        event.preventDefault();
+        if(message){
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+
+    }
+
+    return (
+        <div className="outerContainer">
+            <div className="container">
+                <InfoBar room={room}/>
+                <Messages messages={messages} name={name}/>
+                <Input message={message} setMessage={ setMessage } sendMessage={ sendMessage } />
+            </div>
+        
+        </div>
+    )
+}
+
+export default Chat;
